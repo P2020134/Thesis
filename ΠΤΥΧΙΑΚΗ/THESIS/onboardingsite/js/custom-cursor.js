1172aa1cd6b51311dc016020ff8aa655407656dc
P2020134 <p20gian@ionio.gr>
2024-07-30 11:13:04 +0300

    custom cursor on the onboarding1.1.html , without the default cursor appearing

--- ΠΤΥΧΙΑΚΗ/THESIS/onboardingsite/js/custom-cursor.js
+++ ΠΤΥΧΙΑΚΗ/THESIS/onboardingsite/js/custom-cursor.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     new kursor({
-        type: 4
-    });
+        type: 4,
+        removeDefaultCursor: true // Whether to remove the default cursor
+    });                          // in some cases the Default Cursor appears 
+    
 });
