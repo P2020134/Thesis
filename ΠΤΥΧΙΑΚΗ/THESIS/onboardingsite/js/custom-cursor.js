747bc82a3a38d8548319190febcb653ec6b8464f
P2020134 <p20gian@ionio.gr>
2024-08-05 13:03:48 +0300

    created media queries for diff resolutions  / changed pxs -> rems

--- ΠΤΥΧΙΑΚΗ/THESIS/onboardingsite/js/custom-cursor.js
+++ ΠΤΥΧΙΑΚΗ/THESIS/onboardingsite/js/custom-cursor.js
@@ -1,7 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-    new kursor({
-        type: 1,
-        removeDefaultCursor: false // Whether to remove the default cursor
-    });                          // in some cases the Default Cursor appears 
-    
+    console.log("DOMContentLoaded event fired");
+    try {
+        new kursor({
+            type: 1,
+            removeDefaultCursor: false // Whether to remove the default cursor
+        });
+        console.log("kursor initialized successfully");
+    } catch (error) {
+        console.error("Error initializing kursor:", error);
+    }
 });
